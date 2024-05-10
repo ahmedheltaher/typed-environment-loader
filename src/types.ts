81c5402f48039203a8de87c0e44f0e2ce3d0e9d6
Ahmed Eltaher <ahmedheltaher@gmail.com>
2024-05-10 18:42:01 +0300

    Added Support for array parsing

--- src/types.ts
+++ src/types.ts
@@ -1,15 +1,35 @@
 export type EnumValues = readonly string[];
 
-export type ConfigurationItem<Type> = BaseConfigurationItem<Type> | EnumConfigurationItem;
+export type ConfigurationItem<Type> = BaseConfigurationItem<Type> | EnumConfigurationItem | ArrayConfiguration<Type>;
 
-interface BaseConfigurationItem<Type> {
+export interface ArrayConfiguration<Type> {
+	type: 'array';
+	items: ArrayItem<Type>;
+	default?: Type[];
+	required?: boolean;
+	name: string;
+}
+
+export type ArrayItem<Type> = AtomicArrayItem<Type> | NestedArrayItem<Type>;
+
+export interface AtomicArrayItem<Type> {
+	type: 'string' | 'number' | 'boolean';
+	default?: Type;
+}
+
+export interface NestedArrayItem<Type> {
+	type: 'array';
+	items: ArrayItem<Type>;
+}
+
+export interface BaseConfigurationItem<Type> {
 	type: 'string' | 'number' | 'boolean';
 	default?: Type;
 	required?: boolean;
 	name: string;
 }
 
-interface EnumConfigurationItem {
+export interface EnumConfigurationItem {
 	type: 'enum';
 	values: EnumValues;
 	default: string;
